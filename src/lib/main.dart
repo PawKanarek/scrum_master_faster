eb0b765712ac3c58d5385a10ba069a685f5d92c8
DESKTOP-SQK3IRP\Raix <paw.kanarek@gmail.com>
2019-07-30 20:17:23 +0200

    init commit

---
+++ src/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class PageModel {
+  Timer timer;
+  bool started = false;
+  int currentPersonTalking = 0;
+  int totalElapsedMiliseconds = 0;
+  bool shouldRestart = true;
+  Map<int, int> peopleStanDupTimes = new Map();
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Skram Master: Faster'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final PageModel pageModel = new PageModel();
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(pageModel: pageModel);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState({this.pageModel});
+  final PageModel pageModel;
+
+  void resetTimer() {
+    setState((){
+      if (pageModel.shouldRestart){
+        pageModel.shouldRestart = false;
+        pageModel.started = true;
+        pageModel.peopleStanDupTimes.clear();
+        pageModel.totalElapsedMiliseconds = 0;
+        pageModel.currentPersonTalking = 0;
+      }
+      if (pageModel.timer != null){
+        pageModel.timer.cancel();
+        pageModel.timer = null;
+      }
+      pageModel.peopleStanDupTimes[pageModel.currentPersonTalking++] = 0;
+      pageModel.timer = new Timer.periodic(new Duration(seconds: 1), timerCallback);
+    });
+  }
+
+  void timerCallback(Timer timer){
+    setState((){
+      pageModel.started = true;
+      int currentIndex = pageModel.currentPersonTalking - 1;
+      int currentTime =  pageModel.peopleStanDupTimes[currentIndex];
+      pageModel.peopleStanDupTimes[currentIndex] = ++currentTime;
+      pageModel.totalElapsedMiliseconds++;
+    });
+  }
+
+  void stop(){
+    setState((){
+      if (pageModel.timer != null){
+        pageModel.timer.cancel();
+        pageModel.timer = null;
+      }
+      pageModel.shouldRestart = true;
+      pageModel.started = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle titleTextStyle = const TextStyle(fontSize: 20, color: Colors.black87);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:Container(
+        constraints:  BoxConstraints.expand(),
+        child: Column(
+          children: <Widget>[
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Text((pageModel.started) 
+            //     ? "Standuper ${pageModel.currentPersonTalking + 1} przegadał ${getFormattedTime(pageModel.currentPersonElapsedMiliseconds)}"
+            //     : 'Poranny stand-up czas rozpocząć',
+            //     textAlign: TextAlign.right, style: titleTextStyle),
+            // ),
+            
+            Row(children: <Widget>[
+              Expanded(
+                child: TimerText(pageModel: pageModel)
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+              child: RaisedButton(
+                onPressed: pageModel.started ? stop : resetTimer,
+                child: Text(pageModel.started ? "Koniec": "Start"),
+              ))
+            ]),
+            Expanded(child: new ListView.builder(
+              itemCount: pageModel.peopleStanDupTimes.length,
+              itemBuilder: (BuildContext context, int index){
+                int value = pageModel.peopleStanDupTimes[index]; 
+                String mood = (value < 61)?":)":(value < 121)?":|":":(";
+                return ListTile(
+                  dense: true,
+                  title: Text("${index + 1}. ${getFormattedTime(value)}", style: new TextStyle(fontSize:13 + value.toDouble()/10)),
+                  subtitle:  Text(mood, style: new TextStyle(fontSize:13))
+                );
+              })
+            )
+          ]),
+      ), 
+      floatingActionButton: FloatingActionButton(
+        onPressed: resetTimer,
+        tooltip: 'Increment',
+        child: pageModel.started ? Icon(Icons.person_add) : Icon(Icons.add), 
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TimerText extends StatefulWidget{
+  TimerText({this.pageModel});
+  final PageModel pageModel;
+  TimerTextState createState() => new TimerTextState(pageModel: pageModel);
+}
+
+class TimerTextState extends State<TimerText>{
+  TimerTextState({this.pageModel});
+  final PageModel pageModel;
+
+  @override
+  Widget build(BuildContext context){
+    TextStyle timerTextStyle = const TextStyle(fontSize: 20, color: Colors.black87);
+    return new Text("Czas standupu: ${getFormattedTime(pageModel.totalElapsedMiliseconds)}", style: timerTextStyle, textAlign: TextAlign.center);
+  }
+}
+
+String getFormattedTime(elapsedMilisceconds){
+    String minutes = (elapsedMilisceconds/ 60).floor().toString().padLeft(2, "0");
+    String seconds = (elapsedMilisceconds % 60).toString().padLeft(2, "0");
+    return "$minutes:$seconds";
+}
